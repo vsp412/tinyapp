41d9f7285336315ab46703ae3a484fb14cf74bf9
Vineet Pande <vsp4991@gmail.com>
2021-02-19 17:02:53 +0000

    wrote comments to help simplify the helpers file

--- helpers.js
+++ helpers.js
@@ -1,18 +1,22 @@
 
+//Set of helper functions
 
-function getUserByEmail(user, users) {
+//looks up and returns the user id from the users object, searches by email
+function getUserByEmail(userEmail, users) {
   for (let i in users) {
-    if (users[i]['email'] === user) {
+    if (users[i]['email'] === userEmail) {
       return users[i]['id'];
     }
   }
   return undefined;
 }
 
+//generates an alphanumeric string. 
 function generateRandomString() {
   return Math.random().toString(36).slice(2).slice(0,7);
 }
 
+//checks if email exists in users object
 function checkIfUserExist(userEmail, users) {
   for (let i in users) {
     if (users[i]['email'] === userEmail) {
@@ -23,7 +27,7 @@ function checkIfUserExist(userEmail, users) {
 }
 
 
-
+//returns password from users object, searches by email, the password is a hashed value so its safe to return it.
 function getUsersPassword(userEmail, users) {
   for (let i in users) {
     if (users[i]['email'] === userEmail) {
@@ -34,7 +38,7 @@ function getUsersPassword(userEmail, users) {
 }
 
 
-
+//returns those set of urls from the url database object which belong to the user having the user id equal to the passed u_id parameter.
 function urlsForUser(u_id, urlDatabase) {
   let userURLS = {};
   for (let i in urlDatabase) {
@@ -45,9 +49,11 @@ function urlsForUser(u_id, urlDatabase) {
   return userURLS;
 }
 
+//checks if a given url exists in url database object
 function checkIfURLExist(shortURL, urlDatabase) {
   const bool = Object.keys(urlDatabase).includes(shortURL);
   return bool;
 }
 
+//exporting all helper functions
 module.exports = { getUserByEmail, generateRandomString, checkIfURLExist, checkIfUserExist, urlsForUser, getUsersPassword };
